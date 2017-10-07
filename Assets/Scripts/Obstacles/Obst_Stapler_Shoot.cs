c89316358a7b2e812aec0712b5721012d86b1cc6
licence dim <licences.dim@uqac.ca>
2017-10-07 01:58:12 -0400

    Ajout Stapler

---
+++ Assets/Scripts/Obstacles/Obst_Stapler_Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_Stapler_Shoot : MonoBehaviour {
+
+    public float speed;
+    Rigidbody2D leBody;
+	// Use this for initialization
+	void Start () {
+        leBody = gameObject.GetComponent<Rigidbody2D>();
+
+        leBody.velocity = transform.up * speed;
+
+        Destroy(gameObject, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
