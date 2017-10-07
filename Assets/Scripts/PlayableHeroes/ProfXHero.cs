e1d4f735af90d811c3a36f26c890118ae936841e
Rafaël Côté <raf87crosby@hotmail.com>
2017-10-07 18:17:23 -0400

    le power du prof x pas prof x fonctionne!

--- Assets/Scripts/PlayableHeroes/ProfXHero.cs
+++ Assets/Scripts/PlayableHeroes/ProfXHero.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class ProfXHero : PlayableHero {
 
+    GameObject[] players;
+    GameObject othPlayer;
+
     public override void Awake()
     {
         base.Awake();
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     public override void Spell1()
     {
         base.Spell1();
-        
 
-        
+        if (gameObject == players[0])
+        {
+            othPlayer = players[1];
+        }
+        else
+        {
+            othPlayer = players[0];
+        }
+
+        othPlayer.GetComponent<PlayableHero>().Reverse();
     }
 }
