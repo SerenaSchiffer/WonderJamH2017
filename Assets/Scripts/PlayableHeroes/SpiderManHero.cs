6ce36c5c02146ae05e9b49a4b43fae89ad92a03d
Rafaël Côté <raf87crosby@hotmail.com>
2017-10-07 14:57:01 -0400

    spider man lance de sa web

--- Assets/Scripts/PlayableHeroes/SpiderManHero.cs
+++ Assets/Scripts/PlayableHeroes/SpiderManHero.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class SpiderManHero : PlayableHero
 {
+    public GameObject webPrefab;
+    public float webSpeed = 25;
+
+    GameObject[] players;
+    GameObject othPlayer;
 
     public override void Awake()
     {
         base.Awake();
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     public override void Spell1()
     {
         base.Spell1();
-        Debug.Log("baboin");
+
+        if (gameObject == players[0])
+        {
+            othPlayer = players[1];
+        }
+        else
+        {
+            othPlayer = players[0];
+        }
+
+        GameObject web = Instantiate(webPrefab);
+
+        Vector3 thisPlayerPosition = transform.position;
+        thisPlayerPosition.x += 0.375f;
+
+        web.transform.position = thisPlayerPosition;
+        web.GetComponent<Rigidbody2D>().velocity = (othPlayer.transform.position - transform.position) * webSpeed;
     }
 
 }
