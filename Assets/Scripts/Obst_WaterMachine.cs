36abe97f4f3aa6afef29bbf27ed50c9ad8501375
licence dim <licences.dim@uqac.ca>
2017-10-06 22:35:09 -0400

    Water Machine et Fix the prefabs

---
+++ Assets/Scripts/Obst_WaterMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_WaterMachine : MonoBehaviour {
+
+    public float jumpHeight = 500;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * jumpHeight);
+        }
+            
+    }
+}
