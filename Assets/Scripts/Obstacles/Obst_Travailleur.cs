23efd66d3b562158cfb9a5f1d5a3493c4a3f325a
licence dim <licences.dim@uqac.ca>
2017-10-07 02:04:03 -0400

    Travailleur (Reste a faire le contact avec le joueur mais sagard travaille deja dans le fichier qui faut)

---
+++ Assets/Scripts/Obstacles/Obst_Travailleur.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_Travailleur : MonoBehaviour {
+
+
+    public float speed = 4.0f;
+    public float maxLeft = 10f;
+    public float maxRight = 10f;
+    public float minTimeReverse = 0.5f;
+    public float maxTimeReverse = 4.0f;
+    private int direction;
+    private float repeatTime = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        maxRight = transform.position.x + maxRight;
+        maxLeft = transform.position.x - maxLeft;
+        int startdirection = Random.Range(-5, 6);
+        if (startdirection < 0)
+            direction = -1;
+        else
+            direction = 1;
+        StartCoroutine(RepeatingFunction());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (direction)
+        {
+            case -1:
+                // Moving Left
+                if (transform.position.x > maxLeft)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
+                }
+                else
+                {
+                    direction = 1;
+                }
+                break;
+            case 1:
+                //Moving Right
+                if (transform.position.x < maxRight)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+                }
+                else
+                {
+                    direction = -1;
+                }
+                break;
+        }
+    }
+
+    IEnumerator RepeatingFunction()
+    {
+        while (true)
+        {
+            //execute code here.
+            if (direction == -1)
+                direction = 1;
+            else
+                direction = -1;
+            repeatTime = Random.Range(minTimeReverse, maxTimeReverse);
+            yield return new WaitForSeconds(repeatTime);
+        }
+    }
+}
