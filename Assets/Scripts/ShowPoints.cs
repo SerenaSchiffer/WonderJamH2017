430a880d74b6966cb0e38270088688d634db74f4
Emile Fortin <sylfschiffer@gmail.com>
2017-10-08 09:27:12 -0400

    Ajout du nombre de points pour joueur 1 et 2 + affichage du score

---
+++ Assets/Scripts/ShowPoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money
+{
+    public static float moneyP1 = 0.00f;
+    public static float moneyP2 = 0.00f;
+}
+
+public class ShowPoints : MonoBehaviour {
+    public Text ui_money_player1;
+    public Text ui_money_player2;
+
+    void Update()
+    {
+        ui_money_player1.text = "PLAYER 1 : " + Money.moneyP1 + "$";
+        ui_money_player2.text = "PLAYER 2 : " + Money.moneyP2 + "$";
+    }
+}
