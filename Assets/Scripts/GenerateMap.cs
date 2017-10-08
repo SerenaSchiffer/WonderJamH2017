16fbc4eec092cee8694a8e090a7c9b023cbe9e91
Tryfe712 <pandre711@gmail.com>
2017-10-08 04:12:16 -0400

    ajout de changement dMétage

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -11,13 +11,12 @@ public class GenerateMap : MonoBehaviour {
     public GameObject[] tableFloor;
     public GameObject player1, player2;
     public int actualFloor = 0;
-    public float test = 1f;
-    public Image brique;
-    float baseImagey;
+    public float test = 0f;
+    public GameObject brickWall;
+    public float targetImagey;
     float height;
     // Use this for initialization
     void Start () {
-        baseImagey = brique.transform.position.y;
         height = 2f * Camera.main.orthographicSize;
         tableFloor = new GameObject[towerSize];
 
@@ -34,26 +33,30 @@ public class GenerateMap : MonoBehaviour {
         {
             case CharacterSelect.Characters.Batcroute:
                 player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Batman"));
-                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                player1.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player1.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.JeanGras:
                 player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Jean"));
-                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                player1.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player1.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.Sauceman:
                 player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Aquaman"));
-                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                player1.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player1.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.Spidercheese:
                 player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_SpiderMan"));
-                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                player1.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player1.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
                 break;
         }
 
@@ -62,51 +65,67 @@ public class GenerateMap : MonoBehaviour {
         {
             case CharacterSelect.Characters.Batcroute:
                 player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Batman"));
-                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                player2.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player2.transform.position = player2.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.JeanGras:
                 player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Jean"));
-                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                player2.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player2.transform.position = player2.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.Sauceman:
                 player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Aquaman"));
-                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                player2.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player2.transform.position = player2.GetComponent<PlayableHero>().spawn.position;
                 break;
 
             case CharacterSelect.Characters.Spidercheese:
                 player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_SpiderMan"));
-                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
                 player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                player2.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+                player2.transform.position = player2.GetComponent<PlayableHero>().spawn.position;
                 break;
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetUp();
+        }
 		if(test > 0)
         {
-            
+            brickWall.SetActive(true);
             test -= Time.deltaTime;
-            float y = Mathf.MoveTowards(brique.transform.position.y, -baseImagey, 40f);
-            brique.transform.position = new Vector3(445, y, 0);
+            float y = Mathf.MoveTowards(brickWall.transform.position.y, targetImagey, 25f);
+            brickWall.transform.position = new Vector3(0, y, 0);
         }
         else
         {
-            test = 1f;
-            GetUp();
+            brickWall.transform.position = new Vector3(0, 0, 0);
+            brickWall.SetActive(false);
         }
 	}
 
     public void GetUp()
     {
         actualFloor++;
-        player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
-        player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+        player1.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+        player1.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
+        player2.GetComponent<PlayableHero>().spawn = tableFloor[actualFloor].transform.GetChild(1);
+        player2.transform.position = player1.GetComponent<PlayableHero>().spawn.position;
         Camera.main.transform.Translate(Vector3.up * height);
+        test = 3f;
+    }
+
+    public void DropWall()
+    {
+
     }
 }
