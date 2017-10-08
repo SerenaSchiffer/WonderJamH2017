1007b298b4db5eaded7c5de3f0cd2c3374c7194c
Emile Fortin <sylfschiffer@gmail.com>
2017-10-08 15:32:05 -0400

    EndScreen affiche score

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class EndScreen : MonoBehaviour {
 
     public GameObject[] characters;
-    
+    public Text textScore;
 	// Use this for initialization
 	void Start () {
         if (Money.moneyP1 > Money.moneyP2)
             characters[(int)CharacterSelect.player1Char].SetActive(true);
         else characters[(int)CharacterSelect.player2Char].SetActive(true);
+
+        textScore.text = Money.moneyP1 + "$ vs "+ Money.moneyP2 +"$";
     }
 	
 	// Update is called once per frame
