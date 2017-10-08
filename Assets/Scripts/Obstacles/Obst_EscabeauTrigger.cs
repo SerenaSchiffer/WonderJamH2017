6821e0a48b3ee369a2a24223f4fde8036a186eff
Patsimd <patsimd@gmail.com>
2017-10-07 22:26:55 -0400

    Escabeau presque finie

---
+++ Assets/Scripts/Obstacles/Obst_EscabeauTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_EscabeauTrigger : MonoBehaviour {
+
+    int cpt = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            int rdm = Random.Range(0, 2);
+            if (rdm == 1)
+            {
+                cpt--;
+                GetComponentInParent<Obst_Escabeau>().ThrowStep();
+                if (cpt == 0)
+                {
+                    Destroy(gameObject);
+                }
+                    
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/Obst_Escabeau_Step.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obst_Escabeau_Step : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
