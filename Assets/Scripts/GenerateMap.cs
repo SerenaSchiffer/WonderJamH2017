8e763310202edab28d43bce363b670810b89ff89
Tryfe712 <pandre711@gmail.com>
2017-10-08 02:56:16 -0400

    avancemenet loop

--- Assets/Scripts/GenerateMap.cs
+++ Assets/Scripts/GenerateMap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateMap : MonoBehaviour {
     public float baseX;
@@ -8,9 +9,16 @@ public class GenerateMap : MonoBehaviour {
     public int towerSize;
     public int numberOfFloor;
     public GameObject[] tableFloor;
-	// Use this for initialization
-	void Start () {
-        float height = 2f * Camera.main.orthographicSize;
+    public GameObject player1, player2;
+    public int actualFloor = 0;
+    public float test = 1f;
+    public Image brique;
+    float baseImagey;
+    float height;
+    // Use this for initialization
+    void Start () {
+        baseImagey = brique.transform.position.y;
+        height = 2f * Camera.main.orthographicSize;
         tableFloor = new GameObject[towerSize];
 
         for (int i = 0; i < towerSize;i++)
@@ -20,10 +28,85 @@ public class GenerateMap : MonoBehaviour {
             newFloor.transform.position = new Vector3(baseX, baseY+i*height, 0);
             tableFloor[i] = newFloor;
         }
-	}
+
+
+        switch (CharacterSelect.player1Char)
+        {
+            case CharacterSelect.Characters.Batcroute:
+                player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Batman"));
+                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                break;
+
+            case CharacterSelect.Characters.JeanGras:
+                player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Jean"));
+                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                break;
+
+            case CharacterSelect.Characters.Sauceman:
+                player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Aquaman"));
+                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                break;
+
+            case CharacterSelect.Characters.Spidercheese:
+                player1 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_SpiderMan"));
+                player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player1.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player1;
+                break;
+        }
+
+
+        switch (CharacterSelect.player2Char)
+        {
+            case CharacterSelect.Characters.Batcroute:
+                player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Batman"));
+                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                break;
+
+            case CharacterSelect.Characters.JeanGras:
+                player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Jean"));
+                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                break;
+
+            case CharacterSelect.Characters.Sauceman:
+                player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_Aquaman"));
+                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                break;
+
+            case CharacterSelect.Characters.Spidercheese:
+                player2 = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Characters/Char_SpiderMan"));
+                player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+                player2.GetComponent<PlayableHero>().currentPlayer = CurrentPlayer.Player2;
+                break;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(test > 0)
+        {
+            
+            test -= Time.deltaTime;
+            float y = Mathf.MoveTowards(brique.transform.position.y, -baseImagey, 40f);
+            brique.transform.position = new Vector3(445, y, 0);
+        }
+        else
+        {
+            test = 1f;
+            GetUp();
+        }
 	}
+
+    public void GetUp()
+    {
+        actualFloor++;
+        player1.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+        player2.transform.position = tableFloor[actualFloor].transform.GetChild(1).position;
+        Camera.main.transform.Translate(Vector3.up * height);
+    }
 }
